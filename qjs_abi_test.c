c390a785c844e6f1ecd72238db3d11f98430ab47
cz-mame <cz.mame@gmail.com>
2026-06-07 16:43:06 +0200

    Add files via upload

--- qjs_abi_test.c
+++ qjs_abi_test.c
@@ -10,6 +10,59 @@
 #endif
 
 typedef JSValue (*ESSQJSCallback)(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv);
+typedef void (*ESSQJSLogCallback)(const char *text);
+
+static ESSQJSLogCallback ess_qjs_log_callback = NULL;
+
+static JSValue ess_qjs_log_bridge(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    const char *text = NULL;
+
+    (void)this_val;
+
+    if (argc > 0)
+        text = JS_ToCString(ctx, argv[0]);
+
+    if (ess_qjs_log_callback)
+        ess_qjs_log_callback(text ? text : "");
+
+    if (text)
+        JS_FreeCString(ctx, text);
+
+    return JS_UNDEFINED;
+}
+
+ESS_QJS_ABI_EXPORT void ess_qjs_set_log_callback(ESSQJSLogCallback cb)
+{
+    ess_qjs_log_callback = cb;
+}
+
+ESS_QJS_ABI_EXPORT int ess_qjs_register_log_function(JSContext *ctx, const char *name)
+{
+    JSValue global;
+    JSValue func;
+    const char *function_name;
+    int ret;
+
+    if (!ctx)
+        return -1;
+
+    function_name = name ? name : "essLog";
+    global = JS_GetGlobalObject(ctx);
+    func = JS_NewCFunction2(ctx, ess_qjs_log_bridge, function_name, 1, JS_CFUNC_generic, 0);
+    if (JS_VALUE_GET_TAG(func) == JS_TAG_EXCEPTION) {
+        JS_FreeValue(ctx, global);
+        return -2;
+    }
+
+    ret = JS_SetPropertyStr(ctx, global, function_name, func);
+    JS_FreeValue(ctx, global);
+
+    if (ret < 0)
+        return -3;
+
+    return 0;
+}
 
 ESS_QJS_ABI_EXPORT int ess_qjs_sizeof_jsvalue(void)
 {
